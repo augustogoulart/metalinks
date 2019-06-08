5443a5b73f170fe48c4799236d2e6711b5f102ff
Gus Goular <augusto@goulart.me>
2019-06-08 12:47:14 -0300

    Move js tests to a dedicated directory

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,17 @@
 module.exports = {
-  // tell Jest to handle `*.vue` files
-  moduleFileExtensions: ["js", "json", "vue"],
-  watchman: false,
   moduleNameMapper: {
-    "^~/(.*)$": "<rootDir>/$1",
-    "^~~/(.*)$": "<rootDir>/$1",
-    "^@/(.*)$": "<rootDir>/$1"
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^vue$': 'vue/dist/vue.common.js'
   },
+  moduleFileExtensions: ['js', 'vue', 'json'],
   transform: {
-    // process js with `babel-jest`
-    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
-    // process `*.vue` files with `vue-jest`
-    ".*\\.(vue)$": "<rootDir>/node_modules/vue-jest"
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest',
   },
-  snapshotSerializers: ["<rootDir>/node_modules/jest-serializer-vue"],
-  collectCoverage: true,
-  collectCoverageFrom: [
-    "<rootDir>/components/**/*.vue",
-    "<rootDir>/pages/*.vue"
+  "collectCoverage": true,
+  "collectCoverageFrom": [
+      "<rootDir>/components/**/*.vue",
+      "<rootDir>/pages/**/*.vue"
   ]
-};
+}
