7ea55c64cb59d5aec8bd331153310b58785ea684
Gus Goular <augusto@goulart.me>
2019-06-12 00:16:03 -0300

    Refactoring render_app view plus tests

--- server/core/tests.py
+++ server/core/tests.py
@@ -1,3 +1,23 @@
-def test_home_get(client):
-    response = client.get('/')
+import pytest
+
+
+@pytest.fixture
+def response(client):
+    """
+    Get render_app response
+    """
+    return client.get('/')
+
+
+def test_render_app_get(response):
+    """
+    GET / must return status code 200
+    """
     assert response.status_code == 200
+
+
+def test_render_app_template_used(response):
+    """
+    Home view should render index.html from Nuxt build
+    """
+    assert 'index.html' in [template.name for template in response.templates]
--- server/urls.py
+++ server/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from server.core import views
+from server.core.views import render_app
 
 urlpatterns = [
-    path('', views.home),
+    path('', render_app),
     path('admin/', admin.site.urls),
 ]
