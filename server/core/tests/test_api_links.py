3a08939ae31b49c6356054a4ff1e800816cf8b61
Gus Goular <augusto@goulart.me>
2019-06-24 22:30:56 -0300

    Add mockup API for links

---
+++ server/core/tests/test_api_links.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+@pytest.fixture
+def response(client):
+    """
+    Get render_app response
+    """
+    return client.get('/api/links/')
+
+
+def test_api_links_get_status_code(response):
+    """
+    GET /api/links/ must return status code 200
+    """
+    assert response.status_code == 200
+
+
+def test_api_links_get_response_content(response):
+    """
+    GET /api/links/ must return list of links
+    """
+    assert b'url' in response.content
+    assert b'category' in response.content
+    assert b'title' in response.content
+    assert b'description' in response.content
--- server/settings.py
+++ server/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
     'server.core',
 ]
 
