8d99b0f4fb654682849886871a5b198ea47b15f2
Gus Goular <augusto@goulart.me>
2019-07-07 00:09:30 -0300

    Add more tests for state object

--- tests/unit/Store.links.spec.js
+++ tests/unit/Store.links.spec.js
@@ -3,7 +3,17 @@ import {state as linkState} from '@/store/links'
 describe('Store:links', () => {
   const state = linkState();
 
-  test('Store has state', () => {
-    expect(state).toBeTruthy()
+  test('Store has state object', () => {
+    expect(state).toBeTruthy();
+    expect(typeof state).toBe('object')
   });
+
+  test('Store state has links', () => {
+    expect(state.links).toBeTruthy()
+  });
+
+  test('Links is array', () => {
+    expect(Array.isArray(['state.links'])).toBe(true)
+  });
+
 });
