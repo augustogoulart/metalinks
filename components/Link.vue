b6e44cc79e5fbef9bf021c323990d7896d5be851
Gus Goular <augusto@goulart.me>
2019-08-06 23:47:35 -0300

    Open links in a new tab

--- components/Link.vue
+++ components/Link.vue
@@ -5,9 +5,10 @@
     </div>
     <div class="mt-4 md:mt-0 md:ml-6">
       <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">{{category}}</div>
-      <a href="#" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">{{title}}</a>
+      <a :href="url" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline"
+         target="_blank">{{title }}</a>
       <p class="mt-2 text-gray-600">{{description}}</p>
-      <a :href="url">{{url}}</a>
+      <a :href="url" target="_blank">{{url}}</a>
     </div>
   </div>
 </template>
